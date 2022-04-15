27bd5b05133a13dde23ca84182f0fef5ab0226d6
Sebastian Rodrigo <76745985+sebstianrodrigo@users.noreply.github.com>
2022-04-14 22:10:38 -0500

    Add files via upload

--- js/whatsApp.js
+++ js/whatsApp.js
@@ -1,29 +1,23 @@
 popupWhatsApp = () => {
   
     let btnClosePopup = document.querySelector('.closePopup');
-    let btnOpenPopup = document.querySelector('.whatsapp-button');
     let popup = document.querySelector('.popup-whatsapp');
     let sendBtn = document.getElementById('send-btn');
     
 
     btnClosePopup.addEventListener("click",  () => {
       popup.classList.toggle('is-active-whatsapp-popup')
-    })
+    })   
     
-    btnOpenPopup.addEventListener("click",  () => {
-      popup.classList.toggle('is-active-whatsapp-popup')
-       popup.style.animation = "fadeIn .6s 0.0s both";
-
-    })
     
     sendBtn.addEventListener("click", () => {
     let nume= document.getElementById('num').value;
     let relnum = nume.replace(/ /g,"%20");
+    let code= document.getElementById('cod').value;
+    let relcode = code.replace(/ /g,"%20");
     let msg = document.getElementById('whats-in').value;
-    let relmsg = msg.replace(/ /g,"%20");
-    console.log(msg);
-    console.log(nume);   
-     window.open('https://api.whatsapp.com/send/?phone=51'+relnum,'&text='+relmsg, '_blank'); 
+    let relmsg = msg.replace(/ /g,"%20"); 
+     window.open('https://api.whatsapp.com/send/?phone='+relcode+relnum,'&text='+relmsg, '_blank'); 
       
     });
   
